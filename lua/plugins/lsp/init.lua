4d1f2eadc65974268968ddcf251439e09d60a822
rudeigerc <rudeigerc@gmail.com>
2024-10-03 20:56:54 +0800

    refactor: stylua

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,6 +1,7 @@
 return {
     {
         'williamboman/mason.nvim',
+        cmd = 'Mason',
         opts = {
             ui = {
                 icons = {
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,19 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        requires = {
-            'nvim-treesitter/nvim-treesitter-refactor',
-        },
-        build = ':TSUpdate',
-        config = function()
-            local configs = require('nvim-treesitter.configs')
-
-            configs.setup({
-                ensure_installed = { 'lua' },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
+  {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {
+      'nvim-treesitter/nvim-treesitter-refactor',
     },
+    build = ':TSUpdate',
+    config = function()
+      local configs = require('nvim-treesitter.configs')
+
+      configs.setup({
+        ensure_installed = { 'lua' },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
